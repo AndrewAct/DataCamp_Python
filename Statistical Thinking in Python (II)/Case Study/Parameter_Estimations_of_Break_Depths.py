6e301f5b7822f4f3448541b0a88db70a1d378df6
Andrew Chen <andrew47ohio@gmail.com>
2020-05-17 00:16:21 -0400

    5/16 Commit

---
+++ Statistical Thinking in Python (II)/Case Study/Parameter_Estimations_of_Break_Depths.py
@@ -0,0 +1,21 @@
+# 5/16/2020
+# Compute the difference of the sample means: mean_diff
+mean_diff = np.mean(bd_2012) - np.mean(bd_1975)
+
+# Get bootstrap replicates of means
+bs_replicates_1975 = draw_bs_reps(bd_1975, np.mean, 10000)
+bs_replicates_2012 = draw_bs_reps(bd_2012, np.mean, 10000)
+
+# Compute samples of difference of means: bs_diff_replicates
+bs_diff_replicates = bs_replicates_2012 - bs_replicates_1975
+
+# Compute 95% confidence interval: conf_int
+conf_int = np.percentile(bs_diff_replicates, [2.5, 97.5])
+
+# Print the results
+print('difference of means =', mean_diff, 'mm')
+print('95% confidence interval =', conf_int, 'mm')
+
+# <script.py> output:
+#     difference of means = 0.22622047244094645 mm
+#     95% confidence interval = [0.05633521 0.39190544] mm
