2a266410a13515774393ca56b717f4135ab7f983
Andrew Chen <andrew47ohio@gmail.com>
2020-05-18 23:55:53 -0400

    5/18 Commit

---
+++ Supervised Learning with Scikit-Learn/Fine-Tunning Your Model/Hold_out_Set_In_Practice_I_Classification.py
@@ -0,0 +1,29 @@
+# 5/18/2020
+# Import necessary modules
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+
+# Create the hyperparameter grid
+c_space = np.logspace(-5, 8, 15)
+param_grid = {'C': c_space, 'penalty': ['l1', 'l2']}
+
+# Instantiate the logistic regression classifier: logreg
+logreg = LogisticRegression()
+
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 42 )
+
+# Instantiate the GridSearchCV object: logreg_cv
+logreg_cv = GridSearchCV(logreg, param_grid, cv = 5)
+
+# Fit it to the training data
+logreg_cv.fit(X_train, y_train)
+
+# Print the optimal parameters and best score
+print("Tuned Logistic Regression Parameter: {}".format(logreg_cv.best_params_))
+print("Tuned Logistic Regression Accuracy: {}".format(logreg_cv.best_score_))
+
+# <script.py> output:
+#     Tuned Logistic Regression Parameter: {'C': 0.4393970560760795, 'penalty': 'l1'}
+#     Tuned Logistic Regression Accuracy: 0.7652173913043478
