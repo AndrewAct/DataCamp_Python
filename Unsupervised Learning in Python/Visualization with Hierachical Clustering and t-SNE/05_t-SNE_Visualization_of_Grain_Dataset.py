8a70275dfbded4d6d8492c9c54a91672a0073f05
Andrew Chen <andrew47ohio@gmail.com>
2020-05-22 16:06:44 -0400

    5/22 Commit

---
+++ Unsupervised Learning in Python/Visualization with Hierachical Clustering and t-SNE/05_t-SNE_Visualization_of_Grain_Dataset.py
@@ -0,0 +1,23 @@
+# 5/22/2020
+# Import TSNE
+# t-SNE refer to t-Distributed Stochastic Neighbor Embedding
+# It is useful to deal with large datasets
+# Calculate similarity score
+# Measuring distance
+from sklearn.manifold import TSNE
+
+# Create a TSNE instance: model
+model = TSNE(learning_rate = 200)
+
+# Apply fit_transform to samples: tsne_features
+tsne_features = model.fit_transform(samples)
+
+# Select the 0th feature: xs
+xs = tsne_features[:,0]
+
+# Select the 1st feature: ys
+ys = tsne_features[:,1]
+
+# Scatter plot, coloring by variety_numbers
+plt.scatter(xs, ys, c= variety_numbers)
+plt.show()
