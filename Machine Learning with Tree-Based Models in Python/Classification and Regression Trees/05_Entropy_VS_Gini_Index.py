2bf986ad1958069ae27422a820774efb96ebbd37
Andrew Chen <andrew47ohio@gmail.com>
2020-05-23 21:42:26 -0400

    5/23 Commit

---
+++ Machine Learning with Tree-Based Models in Python/Classification and Regression Trees/05_Entropy_VS_Gini_Index.py
@@ -0,0 +1,24 @@
+# # 5/23/2020
+# In this exercise you'll compare the test set accuracy of dt_entropy to the accuracy of another tree named dt_gini. 
+# The tree dt_gini was trained on the same dataset using the same parameters except for the information criterion which was set to the gini index using the keyword 'gini'.
+
+# X_test, y_test, dt_entropy, as well as accuracy_gini which corresponds to the test set accuracy achieved by dt_gini are available in your workspace.
+
+# Import accuracy_score from sklearn.metrics
+from sklearn.metrics import accuracy_score
+
+# Use dt_entropy to predict test set labels
+y_pred = dt_entropy.predict(X_test)
+
+# Evaluate accuracy_entropy
+accuracy_entropy = accuracy_score(y_test, y_pred)
+
+# Print accuracy_entropy
+print('Accuracy achieved by using entropy: ', accuracy_entropy)
+
+# Print accuracy_gini
+print('Accuracy achieved by using the gini index: ', accuracy_gini)
+
+# <script.py> output:
+#     Accuracy achieved by using entropy:  0.929824561404
+#     Accuracy achieved by using the gini index:  0.929824561404
