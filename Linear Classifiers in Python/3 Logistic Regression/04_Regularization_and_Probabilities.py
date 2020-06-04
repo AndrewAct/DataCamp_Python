b0a837c7cc05e1eefe201fe5f27f493fc8c809cd
Andrew Chen <andrew47ohio@gmail.com>
2020-06-03 22:18:47 -0400

    6/2 Commit

---
+++ Linear Classifiers in Python/3 Logistic Regression/04_Regularization_and_Probabilities.py
@@ -0,0 +1,18 @@
+# # 6/3/2020
+# In this exercise, you will observe the effects of changing the regularization strength on the predicted probabilities.
+
+# A 2D binary classification dataset is already loaded into the environment as X and y
+
+# Set the regularization strength
+model = LogisticRegression(C=0.1)
+
+# Fit and plot
+model.fit(X,y)
+plot_classifier(X,y,model,proba=True)
+
+# Predict probabilities on training points
+prob = model.predict_proba(X)
+print("Maximum predicted probability", np.max(prob))
+
+# <script.py> output:
+#     Maximum predicted probability 0.8990965659596716
