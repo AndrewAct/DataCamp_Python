4801133ce82998960d261fab4e61efaa2218a748
Andrew Chen <andrew47ohio@gmail.com>
2020-06-13 23:48:03 -0400

    6/13 Commit

---
+++ Dimensionality Reduction in Python/1 Exploring High Dimensionality Data/03_Fitting_t-SNE_to_the_ANSUR_Data.py
@@ -0,0 +1,18 @@
+# # 6/13/2020
+# t-SNE is a great technique for visual exploration of high dimensional datasets. In this exercise, you'll apply it to the ANSUR dataset. You'll remove non-numeric columns from the pre-loaded dataset df and fit TSNE to his numeric dataset.
+
+# Non-numerical columns in the dataset
+non_numeric = ['Branch', 'Gender', 'Component']
+
+# Drop the non-numerical columns from df
+df_numeric = df.drop(non_numeric, axis= 1)
+
+# Create a t-SNE model with learning rate 50
+m = TSNE(learning_rate= 50)
+
+# Fit and transform the t-SNE model on the numeric dataset
+tsne_features = m.fit_transform(df_numeric)
+print(tsne_features.shape)
+
+# <script.py> output:
+#     (6068, 2)
