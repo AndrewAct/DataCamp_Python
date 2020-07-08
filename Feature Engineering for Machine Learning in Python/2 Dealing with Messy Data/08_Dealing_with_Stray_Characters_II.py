af1dfa1f4661229cdd4daa53b0e7e3085e2193a7
Andrew Chen <andrew47ohio@gmail.com>
2020-07-07 23:25:57 -0400

    7/7 Commit

--- Feature Engineering for Machine Learning in Python/2 Dealing with Messy Data/08_Dealing_with_Stray_Characters_II.py
+++ Feature Engineering for Machine Learning in Python/2 Dealing with Messy Data/08_Dealing_with_Stray_Characters_II.py
@@ -78,3 +78,76 @@ print(so_survey_df['RawSalary'][idx])
 #     994           NaN
 #     997           NaN
 #     Name: RawSalary, Length: 401, dtype: object
+
+# Replace the offending characters
+so_survey_df['RawSalary'] = so_survey_df['RawSalary'].str.replace('£','')
+
+# Convert the column to float
+so_survey_df['RawSalary'] = so_survey_df['RawSalary'].astype('float')
+
+# Print the column
+print(so_survey_df['RawSalary'])
+
+# <script.py> output:
+#     0            NaN
+#     1        70841.0
+#     2            NaN
+#     3        21426.0
+#     4        41671.0
+#     5       120000.0
+#     6            NaN
+#     7       250000.0
+#     8            NaN
+#     9            0.0
+#     10       47904.0
+#     11           NaN
+#     12       95968.0
+#     13           NaN
+#     14         420.0
+#     15       75000.0
+#     16       10958.0
+#     17       51408.0
+#     18       72611.0
+#     19      900000.0
+#     20           NaN
+#     21       30000.0
+#     22           NaN
+#     23       44000.0
+#     24       60000.0
+#     25           NaN
+#     26       80000.0
+#     27           NaN
+#     28           NaN
+#     29           NaN
+#              ...    
+#     969      37200.0
+#     970      79973.0
+#     971      73428.0
+#     972      56298.0
+#     973      17628.0
+#     974     125000.0
+#     975          NaN
+#     976          NaN
+#     977          NaN
+#     978      75000.0
+#     979       6576.0
+#     980          NaN
+#     981      60000.0
+#     982      80000.0
+#     983      90000.0
+#     984      70000.0
+#     985      39648.0
+#     986      99967.0
+#     987       2352.0
+#     988      50448.0
+#     989          NaN
+#     990          NaN
+#     991      55562.0
+#     992          NaN
+#     993      30000.0
+#     994          NaN
+#     995      58746.0
+#     996      55000.0
+#     997          NaN
+#     998    1000000.0
+#     Name: RawSalary, Length: 999, dtype: float64
