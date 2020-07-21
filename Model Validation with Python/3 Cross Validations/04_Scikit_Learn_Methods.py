1ce8084a9f8abcc2ea27eb68648972548fb2aecf
Andrew Chen <andrew47ohio@gmail.com>
2020-07-20 23:37:55 -0400

    Delete 04_Scikit_Learn_Methods.py

--- Model Validation with Python/3 Cross Validations/04_Scikit_Learn_Methods.py
+++
@@ -1,9 +0,0 @@
-# # 7/17/2020
-# You have decided to build a regression model to predict the number of new employees your company will successfully hire next month. You open up a new Python script to get started, but you quickly realize that sklearn has a lot of different modules. Let's make sure you understand the names of the modules, the methods, and which module contains which method.
-
-# Follow the instructions below to load in all of the necessary methods for completing cross-validation using sklearn. You will use modules:
-
-You have decided to build a regression model to predict the number of new employees your company will successfully hire next month. You open up a new Python script to get started, but you quickly realize that sklearn has a lot of different modules. Let's make sure you understand the names of the modules, the methods, and which module contains which method.
-
-Follow the instructions below to load in all of the necessary methods for completing cross-validation using sklearn. You will use modules:
-
