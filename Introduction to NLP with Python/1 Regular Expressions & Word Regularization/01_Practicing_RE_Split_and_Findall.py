4b34d9682b9fadb951c48be5c5b67e30a713b082
Andrew Chen <andrew47ohio@gmail.com>
2020-07-22 23:29:48 -0400

    7/22 Commit

---
+++ Introduction to NLP with Python/1 Regular Expressions & Word Regularization/01_Practicing_RE_Split_and_Findall.py
@@ -0,0 +1,33 @@
+# # 7/22/2020
+# Now you'll get a chance to write some regular expressions to match digits, strings and non-alphanumeric characters. Take a look at my_string first by printing it in the IPython Shell, to determine how you might best match the different steps.
+
+# Note: It's important to prefix your regex patterns with r to ensure that your patterns are interpreted in the way you want them to. Else, you may encounter problems to do with escape sequences in strings. For example, "\n" in Python is used to indicate a new line, but if you use the r prefix, it will be interpreted as the raw string "\n" - that is, the character "\" followed by the character "n" - and not as a new line.
+
+# The regular expression module re has already been imported for you.
+
+# Remember from the video that the syntax for the regex library is to always to pass the pattern first, and then the string second.
+
+# Write a pattern to match sentence endings: sentence_endings
+sentence_endings = r"[.?!]"
+
+# Split my_string on sentence endings and print the result
+print(re.split(sentence_endings, my_string))
+
+# Find all capitalized words in my_string and print the result
+capitalized_words = r"[A-Z]\w+"
+print(re.findall(capitalized_words, my_string))
+
+# Split my_string on spaces and print the result
+spaces = r"\s+"
+print(re.split(spaces, my_string))
+
+# Find all digits in my_string and print the result
+digits = r"\d+"
+print(re.findall(digits, my_string))
+
+
+# <script.py> output:
+#     ["Let's write RegEx", "  Won't that be fun", '  I sure think so', '  Can you find 4 sentences', '  Or perhaps, all 19 words', '']
+#     ['Let', 'RegEx', 'Won', 'Can', 'Or']
+#     ["Let's", 'write', 'RegEx!', "Won't", 'that', 'be', 'fun?', 'I', 'sure', 'think', 'so.', 'Can', 'you', 'find', '4', 'sentences?', 'Or', 'perhaps,', 'all', '19', 'words?']
+#     ['4', '19']
