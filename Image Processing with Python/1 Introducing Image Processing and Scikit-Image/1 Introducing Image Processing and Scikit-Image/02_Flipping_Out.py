241746a70ee4774f11f6cd49a200c70bb11fd92f
Andrew Chen <andrew47ohio@gmail.com>
2020-08-12 23:44:49 -0400

    8/12 Commit

---
+++ Image Processing with Python/1 Introducing Image Processing and Scikit-Image/1 Introducing Image Processing and Scikit-Image/02_Flipping_Out.py
@@ -0,0 +1,20 @@
+# # 8/11/2020
+# As a prank, someone has turned an image from a photo album of a trip to Seville upside-down and back-to-front! Now, we need to straighten the image, by flipping it.
+
+# Using the NumPy methods learned in the course, flip the image horizontally and vertically. Then display the corrected image using the show_image() function.
+# NumPy is already imported as np.
+
+# Flip the image vertically.
+
+# Flip the image vertically
+seville_vertical_flip = np.flipud(flipped_seville)
+
+# Now, flip the vertically-flipped image horizontally.
+# Flip the image vertically
+seville_vertical_flip = np.flipud(flipped_seville)
+
+# Flip the image horizontally
+seville_horizontal_flip = np.fliplr(seville_vertical_flip)
+
+# Show the resulting image
+show_image(seville_horizontal_flip, 'Seville')
